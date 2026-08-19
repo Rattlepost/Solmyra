3b30cb061c2737379692788716b39229f5c0c7bc
Ashton <130703127+Rattlepost@users.noreply.github.com>
2026-08-19 15:40:30 -0500

    added pellhiem map and other fixes

---
+++ regions/pellhiem.js
@@ -0,0 +1,16 @@
+const PELLHIEM = {
+  map: "images/pellhiem.png",
+  viewBox: "0 0 3326 4193", // ← update if your map is a different size
+
+  regions: [
+    {
+      id: "duskhelm",
+      name: "Duskhelm",
+      type: "Capital City",
+      grad: "rg_poi",
+      pts: "", 
+      wikiLink: "wiki/regions/grennmoor/towns/duskhelm.html",
+      summary: "The capital city of Grennmoor, known for its canals and stone architecture. It is ruled by the Council of Duskhelm."            // ← paste your traced polygon coords here
+    },
+  ]
+}
