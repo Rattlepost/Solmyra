4eaa828a41865f957c30dd1f800745e86edea525
Ashton <130703127+Rattlepost@users.noreply.github.com>
2026-08-21 22:47:54 -0500

    added poi hovers to pellhiem map

--- regions/pellhiem.js
+++ regions/pellhiem.js
@@ -4,13 +4,130 @@ const PELLHIEM = {
 
   regions: [
     {
-      id: "duskhelm",
-      name: "Duskhelm",
-      type: "Capital City",
+      id: "fort_eldwick",
+      name: "Fort Eldwick",
+      type: "Fort",
       grad: "rg_poi",
-      pts: "", 
-      wikiLink: "wiki/regions/solmarch/pellhiem/towns/duskhelm.html",
-      summary: "The capital city of Grennmoor, known for its canals and stone architecture. It is ruled by the Council of Duskhelm."            // ← paste your traced polygon coords here
+      pts: "1539,1674,110", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/pois/fort_eldwick.html",
+      summary: "Abandoned by Grennmoor over 200 years ago, the reclaimed ruins of Fort Eldwick serve as Pellhiem's mining center."
+    },
+    {
+      id: "pell_castle",
+      name: "Pell Castle",
+      type: "Castle",
+      grad: "rg_poi",
+      pts: "1965,2473,91", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/pell_castle.html",
+      summary: "The grand castle of Pellhiem. The seat of power and home for the ruling family."
+    },
+    {
+      id: "Varhold_monestary",
+      name: "Varhold Monestary",
+      type: "Religious Site",
+      grad: "rg_poi",
+      pts: "1245,3441,107", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/pois/varhold_monestary.html",
+      summary: "A monestary dedicated to the worship of Valreth, Pellhiem's main god of worship."
+    },
+    {
+      id: "Lakeside_district",
+      name: "Lakeside District",
+      type: "District",
+      grad: "rg_poi",
+      pts: "1774,2440,69", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/lakeside_district.html",
+      summary: "The lakeside district is a peaceful area known for its scenic views. It's Pellhiem's most wealthy district."
+    },
+    {
+      id: "Lesser_district",
+      name: "Lesser District",
+      type: "District",
+      grad: "rg_poi",
+      pts: "2191,2498,90", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/lesser_district.html",
+      summary: "The lesser district is where most of the impoverished residents of Pellhiem live."
+    },
+    {
+      id: "Red_district",
+      name: "Red District",
+      type: "District",
+      grad: "rg_poi",
+      pts: "2036,2671,84", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/red_district.html",
+      summary: "Sitting on the border of the Red forest, the Red District is Pellhiem's second oldest district."
+    },
+    {
+      id: "Founding_district",
+      name: "Founding District",
+      type: "District",
+      grad: "rg_poi",
+      pts: "1683,2691,88", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/founding_district.html",
+      summary: "The founding district is where Pellhiem was originally established. It's the oldest district in the city."
+    },
+    {
+      id: "Spanning_district",
+      name: "Spanning District",
+      type: "District",
+      grad: "rg_poi",
+      pts: "1287,2665,99", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/spanning_district.html",
+      summary: "Pellhiem's expansionary district, reaching far to the west. It is the newest and fastest growing district in the city."
+    },
+    {
+      id: "Southern_farmland",
+      name: "Southern Farmland",
+      type: "District",
+      grad: "rg_poi",
+      pts: "1401,2957,158", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/southern_farmland.html",
+      summary: "The southern farmland is a fertile area known for its cattle and crops. It's the original and primary source of food for the city."
+    },
+    {
+      id: "Northern_farmland",
+      name: "Northern Farmland",
+      type: "District",
+      grad: "rg_poi",
+      pts: "2177,2149,144", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/districts/northern_farmland.html",
+      summary: "The northern farmland is less suited for cattle and mostly comprised of crops. It's a quieter district, with a more rural feel."
+    },
+    {
+      id: "mavore_savannah",
+      name: "Mavore Savannah",
+      type: "District",
+      grad: "rg_poi",
+      pts: "2701,2086,251", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/pois/mavore_savannah.html",
+      summary: "A vast grassy plains, streaching from Pellhiem to the eastern shore of Solmarch."            // ← paste your traced polygon coords here
+    },
+    {
+      id: "Red_forest",
+      name: "Red Forest",
+      type: "District",
+      grad: "rg_poi",
+      pts: "2531,3213,411", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/pois/red_forest.html",
+      summary: "A dense forest of ancient trees, home to many creatures and a source of valuable timber."            // ← paste your traced polygon coords here
+    },
+    {
+      id: "hellspur_mountians",
+      name: "Hellspur Mountains",
+      type: "District",
+      grad: "rg_poi",
+      pts: "1003,2001,366", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/pois/hellspur_mountians.html",
+      summary: "A range of rugged mountains, home to many wyrms and other dangerous creatures. It is also a source of precious minerals and metals."            // ← paste your traced polygon coords here
+    },
+    {
+      id: "hellspur_mountians_2",
+      name: "Hellspur Mountains",
+      type: "District",
+      grad: "rg_poi",
+      pts: "2003,1392,334", 
+      wikiLink: "wiki/regions/solmarch/pellhiem/pois/hellspur_mountians.html",
+      summary: "A range of rugged mountains, home to many wyrms and other dangerous creatures. It is also a source of precious minerals and metals."            // ← paste your traced polygon coords here
     },
   ]
 }
