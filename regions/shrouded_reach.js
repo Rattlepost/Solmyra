c19c9916e7d9b9e5155dd53b63c940bc8ed7b2ed
Ashton McGlone <amcglone4626@outlook.com>
2026-08-24 14:40:45 -0500

    added shrouded reach map

---
+++ regions/shrouded_reach.js
@@ -0,0 +1,17 @@
+const SHROUDED_REACH = {
+  map: "images/shrouded_reach.webp",
+  viewBox: "0 0 2941 2609", // ← update if your map is a different size
+
+  regions: [
+    {
+      id: "fort_eldwick",
+      name: "Fort Eldwick",
+      type: "Fort",
+      grad: "rg_poi",
+      pts: "",
+      wikiLink: "wiki/regions/solmarch/pellhiem/pois/fort_eldwick.html",
+      summary:
+        "Abandoned by Grennmoor over 200 years ago, the reclaimed ruins of Fort Eldwick serve as Pellhiem's mining center.",
+    },
+  ],
+};
